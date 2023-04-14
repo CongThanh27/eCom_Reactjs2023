90957f16e9b54a57d2b9d07885b26d31de6598df
ducanh1811 <119037853+ducanh1811@users.noreply.github.com>
2023-04-14 15:30:12 +0700

    update 02

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { Home, Pay, CheckLogin, Layout, Login, Details } from "./Component";
-
 //import PrivateRouter from './Component/BaiTapBuoi6/PrivateRouter';
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
